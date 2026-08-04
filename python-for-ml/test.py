e7ed7b597a5f56298e763adf6a493a3d835fa795
Shaimaa Atef <shaimaaatef845@gmail.com>
2026-08-04 05:09:37 +0300

    for testing

--- python-for-ml/test.py
+++ python-for-ml/test.py
@@ -1,1 +1,7 @@
-print("Hello Machine Learning")
+name = input("what your name:")
+age  = input("what your age:")
+course = input("what your courcs:")
+
+print("my name",name )
+print("my age",age )
+print("course is ",course )
